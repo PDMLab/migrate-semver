6bb0a8863f4880691db66bb627cef6a62f440db8
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2016-08-24 23:40:18 +0200

    Validate migration 0.1.0 -> 0.3.0 works

--- test/index.js
+++ test/index.js
@@ -172,4 +172,40 @@ describe('Migrations', () => {
       });
     });
   });
+
+  describe('When running migration from existing migration table from 0.1.0 to 0.3.0', () => {
+    it('should contain 3 migrations in migrations table', done => {
+      let version = '0.1.0';
+      const migrationsDirectory = path.join(__dirname, 'migrations', 'none-0.1.0-0.3.0');
+      const migrateSemVer = new SemVerMigration({ migrationsDirectory }, fakePlugin());
+
+      migrateSemVer.connect({}, err => { // eslint-disable-line
+        migrateSemVer.up({ version }, err => { // eslint-disable-line
+          assert.equal(migrations[0].version, version);
+          version = '0.3.0';
+          migrateSemVer.up({ version }, err => { // eslint-disable-line
+            assert.equal(migrations[2].version, version);
+            done();
+          });
+        });
+      });
+    });
+
+    it('should create 5 tables in database', done => {
+      let version = '0.1.0';
+      const migrationsDirectory = path.join(__dirname, 'migrations', 'none-0.1.0-0.3.0');
+      const migrateSemVer = new SemVerMigration({ migrationsDirectory }, fakePlugin());
+
+      migrateSemVer.connect({}, err => { // eslint-disable-line
+        migrateSemVer.up({ version }, err => { // eslint-disable-line
+          version = '0.3.0';
+          assert.equal(tables.length, 3);
+          migrateSemVer.up({ version }, err => { // eslint-disable-line
+            assert.equal(tables.length, 5);
+            done();
+          });
+        });
+      });
+    });
+  });
 });
